18dc50c01b211d9dbea9c0302f6a3f24e1981d3d
duutabib <k.aduut@gmail.com>
2025-08-25 21:20:08 +0100

    Reorganize repository structure...

--- src/preprocessor.py
+++
@@ -1,9 +0,0 @@
-import pdfplumber
-
-
-def preprocess_pdf(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
-            text = page.extract_text()
-            return text
-    
