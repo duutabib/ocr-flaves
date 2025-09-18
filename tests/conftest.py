6e1bec141e3522fd643b09e12bc65124330743b3
duutabib <k.aduut@gmail.com>
2025-09-18 19:04:46 +0100

    refactor: add imports, fix function,...

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,21 @@
 import pytest
 import os
 from pathlib import Path
-from config import DEFAULT_DATA_PATH
+from staging.config import DEFAULT_DATA_PATH
 
-# Add the project root to the Python path
-PROJECT_ROOT = Path(__file__).parent.parent
-test_data_dir = DEFAULT_DATA_PATH
 
 @pytest.fixture
 def test_data_dir():
     """Fixture to get the path to the test data directory."""
-    return os.path.join(PROJECT_ROOT, "tests", "test_data")
+    return os.path.join(DEFAULT_DATA_PATH)
 
 @pytest.fixture
 def sample_pdf_path(test_data_dir):
     """Fixture to get the path to a sample PDF file."""
-    return os.path.join(test_data_dir,"invoice.pdf")
+    return os.path.join(test_data_dir, "invoice0.pdf")
 
 @pytest.fixture
 def sample_image_path(test_data_dir):
     """Fixture to get the path to a sample image file."""
     return os.path.join(test_data_dir, "invoice1.jpg")
 
-# Add more fixtures as needed
